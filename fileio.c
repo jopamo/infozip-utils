2d5a94b0e7d2fdbba49bffe3943d3dcba32258b1
Steven M. Schweda <sms@antinode.info>
2025-10-18 17:22:32 -0500

    Fix lame code in fileio.c

--- fileio.c
+++ fileio.c
@@ -2472,6 +2472,7 @@ zusz_t makeint64(sig)
      */
     return (((zusz_t)sig[7]) << 56)
         + (((zusz_t)sig[6]) << 48)
+        + (((zusz_t)sig[5]) << 40)
         + (((zusz_t)sig[4]) << 32)
         + (zusz_t)((((ulg)sig[3]) << 24)
                  + (((ulg)sig[2]) << 16)
